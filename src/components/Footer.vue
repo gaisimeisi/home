0524a10e215467d910ebd68ec67576ed269c1b0d
gaisimeisi <32133368+gaisimeisi@users.noreply.github.com>
2024-12-02 23:44:59 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,10 +15,6 @@
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
-          </a>
         </span>
         <!-- 站点备案 -->
         <span>
